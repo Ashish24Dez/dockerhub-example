bece55ad12726f510fc86bb59648711a9562c22f
Ashish24Dez <69496271+Ashish24Dez@users.noreply.github.com>
2021-08-28 23:23:32 +0530

    Update push.sh

--- jenkins/push.sh
+++ jenkins/push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker push https://hub.docker.com/repository/docker/ashishut/appdep/myimage/dp-alpine:latest
+sudo docker push docker.io/repository/docker/ashishut/appdep/myimage/dp-alpine:latest
